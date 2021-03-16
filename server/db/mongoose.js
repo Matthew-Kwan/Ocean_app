25c02b044fec5e2ec1ec07fe7912bc236d0476d2
Matthew-Kwan <matthew.k913@gmail.com>
2021-03-16 15:53:02 -0400

    connected DB to server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,13 +2,18 @@
    our mongo server through the Mongoose API.
    We will access the connection in our express server. */
 const mongoose = require('mongoose')
+require('../.env')
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentAPI'
+console.log(process.env.MONGODB_URI)
+const mongoURI = process.env.MONGODB_URI
 
 mongoose.connect(mongoURI,
 	{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+  .then((data) => {
+    console.log('Successfully connected to mongodb!')
+  })
 	.catch((error) => {
 		console.log('Error connecting to mongodb. Timeout reached.')
 	})
---
+++ server/server.js
@@ -0,0 +1,14 @@
+/* server.js, with mongodb API */
+'use strict';
+require('dotenv').config()
+
+const log = console.log
+const path = require('path')
+
+const express = require('express')
+// starting the express server
+const app = express();
+
+// mongoose and mongo connection
+const { mongoose } = require('./db/mongoose')
+mongoose.set('bufferCommands', false);  // don't buffer db requests if the db server isn't connected - minimizes http requests hanging if this is the case.
