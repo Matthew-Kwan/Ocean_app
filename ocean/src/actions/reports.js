adffa9fefe8685c92884e69ae845d029531a4bb4
Ling Lu <ling.lu@hotmail.ca>
2021-04-05 17:05:00 -0400

    report CONNECTIONZ

--- ocean/src/actions/reports.js
+++ ocean/src/actions/reports.js
@@ -78,3 +78,51 @@ export const addReport = (report) => {
             console.log(error);
         });
 };
+
+export const markReviewed = (report) => {
+    // the URL for the request
+    const url = `${API_HOST}/api/reports/${report._id}`;
+
+    // The data we are going to send in our request
+    // const student = formComp.state
+
+    // Create our request constructor with all the parameters we need
+    const request = new Request(url, {
+        method: "patch",
+        body: JSON.stringify(report),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+
+    // Send the request with fetch()
+    fetch(request)
+        .then(function (res) {
+            // Handle response we get from the API.
+            // Usually check the error codes to see what happened.
+            if (res.status === 200) {
+                // If student was added successfully, tell the user.
+                console.log('report succesfully patched')
+                // dashboardComp.setState({
+                //     message: {
+                //         body: "Success: Added a student.",
+                //         type: "success"
+                //     }
+                // });
+            } else {
+                console.log('report failed to mark as reviewd')
+                // If server couldn't add the student, tell the user.
+                // Here we are adding a generic message, but you could be more specific in your app.
+                // dashboardComp.setState({
+                //     message: {
+                //         body: "Error: Could not add student.",
+                //         type: "error"
+                //     }
+                // });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
