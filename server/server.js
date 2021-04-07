6ec1eb3e3786ed8fc05bf5552a15b5a9db2c9856
Ling Lu <ling.lu@hotmail.ca>
2021-04-07 16:53:29 -0400

    pls work

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ const cors = require('cors')
 // starting the express server
 const app = express();
 
+
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../ocean/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../ocean/build/index.html'))
+})
 // start cors
 app.use(cors())
 
