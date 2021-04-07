1fabe5a1d63dc4f5296a2953926c3a35a562a07e
Ling Lu <ling.lu@hotmail.ca>
2021-04-07 17:49:38 -0400

    minor edit server

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../ocean/build')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../ocean/build/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/../ocean/build/index.html'))
+// })
 // start cors
 app.use(cors())
 
